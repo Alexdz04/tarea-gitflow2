989d95a6f4bfd8b539f5a0ff852ba9f2528d24ca
Alejandro Diaz Peralta <alejandrodiazdp1@gmail.com>
2024-11-23 14:02:23 -0400

    Confirmando estado funcional.....

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Holaaaa");
+        Console.WriteLine("Adios desde la rama 'feature/agregar-despedida'");
+        Console.WriteLine("Este es un saludo desde la rama 'feature/agregar-saludo'.");
+    }
+}
