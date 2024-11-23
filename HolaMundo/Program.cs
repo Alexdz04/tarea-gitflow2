45961227d8792f162fc5055b4e5f3ce2630019de
Alejandro Diaz Peralta <alejandrodiazdp1@gmail.com>
2024-11-23 14:20:19 -0400

    Cambiado formato de la salida feature formato salida

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {Console.WriteLine("***********************");
+        Console.WriteLine("Holaaaa");
+        Console.WriteLine("Adios desde la rama 'feature/agregar-despedida'");
+        Console.WriteLine("Este es un saludoo desde la rama 'feature/agregar-saludo'.");
+        Console.WriteLine("***********************");
+        Console.WriteLine("Este saludo fue mejorado en la rama 'feature/mejorar-saludo'.");
+    }
+}
