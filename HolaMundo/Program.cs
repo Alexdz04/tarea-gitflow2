3174b3ed848348d9897b1dfee034d17e1d9da8b3
Alejandro Diaz Peralta <alejandrodiazdp1@gmail.com>
2024-11-23 14:22:39 -0400

    Agregado una pequeña interaccion con usuario en agregar-usuario

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {Console.WriteLine("***********************");
+        Console.WriteLine("Holaaaa");
+        Console.WriteLine("Adios desde la rama 'feature/agregar-despedida'");
+        Console.WriteLine("Este es un saludoo desde la rama 'feature/agregar-saludo'.");
+        Console.WriteLine("***********************");
+        Console.WriteLine("Este saludo fue mejorado en la rama 'feature/mejorar-saludo'.");
+
+        string nombre = Console.ReadLine();
+        Console.WriteLine($"Holaaaaa {nombre}! Bienvenidoooo.");
+        Console.WriteLine("Adios otra vez desde la rama feature/agregar-despedida!");
+    }
+}
