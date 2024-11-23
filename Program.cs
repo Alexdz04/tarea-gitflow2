2c46eb7ccd544d227daa3a2438cb48ef126de73d
Alejandro Diaz Peralta <alejandrodiazdp1@gmail.com>
2024-11-23 13:48:08 -0400

    Agregue un saludo en feature/agregar-saludo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Holaaaaa");
+        Console.WriteLine("Este es el saludo desde la rama ...:> 'feature/agregar-saludo'.");
+    }
+}
