aeda22d2cb1f89c59b142a35192b3f62f4fa6faa
Alejandro Diaz Peralta <alejandrodiazdp1@gmail.com>
2024-11-23 14:15:20 -0400

    mejorado el saludo feature mejorar saludo

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Holaaaa");
+        Console.WriteLine("Adios desde la rama 'feature/agregar-despedida'");
+        Console.WriteLine("Este es un saludoo desde la rama 'feature/agregar-saludo'.");
+        Console.WriteLine("Este saludo fue mejorado en la rama 'feature/mejorar-saludo'.");
+    }
+}
